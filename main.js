667b796ed17defda87c51a9604db81de49663741
clarisse guiadem <clarisseguiadem071@gmail.com>
2023-04-02 13:05:57 +0200

    delete button eingestellt card mit JS view function gemacht

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const fetchUrl = "https://jsonplaceholder.typicode.com";
 
+let heroes_div = document.querySelector(".heroes");
+
+
 let posts = [];
+let comments = [];
+let postID;
 
 //get Post
 
@@ -9,15 +14,126 @@ async function getPosts() {
     return await response.json();
 }
 
-//display card
+async function getComments(postID) {
+    const response = await fetch(`${fetchUrl}/posts/${postID}/comments`)
+    return await response.json();
+}
 
-async function displayCard (){
+async function getID (postID) {
+    const response = await fetch(`${fetchUrl}/posts/${postID}`)
+    return await response.json();
+}
+
+//display card-grid
+
+async function displayCard() {
     posts = await getPosts();
 
     console.log(posts);
+    welcomeText();
     createCard();
 }
 displayCard();
+
+
+//display card
+async function viewCard(e) {
+    let parentDiv = document.querySelector("#col2");
+    let commentDiv = document.querySelector("#col1");
+
+    const id = +e.target.dataset.id;
+    comments = await getComments(id);
+   const viewPost = posts.find(item => item.id === id);
+   console.log(viewPost);
+   console.log(comments);
+
+
+showCardHead();
+    showCardFooter();
+    parentDiv.style.display = 'none';
+    commentDiv.style.display = 'block';
+    heroes_div.style.display = 'none';
+
+}
+
+
+
+//delete post
+
+function deleteButton(e) {
+   // const id = +e.target.dataset.id;
+e.target.parentElement.parentElement.remove();
+
+    //console.log(postID);
+}
+
+
+//show welcome text
+const welcomeText = () => {
+
+    const p_heroes = document.createElement("p");
+    p_heroes.textContent = `Welcome to my blog`;
+
+    heroes_div.appendChild(p_heroes);
+}
+
+
+//create Card layout
+
+
+
+
+
+
+const showCardHead = () => {
+    posts.forEach(item => {
+        const parentDivCard = document.createElement("div");
+        const h3 = document.createElement("h3");
+        h3.classList.add("heroes");
+        h3.textContent = item.title;
+
+        const p = document.createElement("p");
+        p.classList.add("card-body");
+        p.textContent = item.body;
+
+        const span = document.createElement("span");
+        span.classList.add("comment");
+        span.textContent = `Comments`;
+
+        parentDivCard.appendChild(h3);
+        parentDivCard.appendChild(p);
+        parentDivCard.appendChild(span);
+    })
+}
+
+
+const showCardFooter = () => {
+    let commentDiv = document.querySelector("#col1");
+
+    comments.forEach(item => {
+        const parentDiv = document.createElement("div");
+        parentDiv.classList.add("display-comments");
+
+        const childDiv = document.createElement("div");
+        childDiv.classList.add("showComment");
+
+        const nameUser = document.createElement("p");
+        nameUser.textContent = item.name;
+
+        const emailUser = document.createElement("i");
+        emailUser.textContent = item.email;
+
+        const body = document.createElement("p");
+        body.textContent = item.body;
+
+        childDiv.appendChild(nameUser);
+        childDiv.appendChild(emailUser);
+        childDiv.appendChild(body);
+
+        parentDiv.appendChild(childDiv);
+        commentDiv.appendChild(parentDiv);
+    })
+}
 
 
 // create Card im Dom
@@ -45,19 +161,17 @@ const createCard = () => {
         view_btn.classList.add("view-btn");
         view_btn.textContent = `View`;
         view_btn.setAttribute("data-id", item.id);
+        view_btn.addEventListener("click", viewCard);
 
-        const edit_btn = document.createElement("button");
-        edit_btn.classList.add("edit-btn");
-        edit_btn.setAttribute("data-id", item.id);
-        edit_btn.textContent = `Edit`;
+
 
         const delete_btn = document.createElement("button");
         delete_btn.classList.add("delete-btn");
         delete_btn.setAttribute("data-id", item.id);
         delete_btn.textContent = `Delete`;
+        delete_btn.addEventListener("click", deleteButton);
 
         btn_div.appendChild(view_btn);
-        btn_div.appendChild(edit_btn);
         btn_div.appendChild(delete_btn);
 
         card.appendChild(title);
@@ -68,3 +182,6 @@ const createCard = () => {
 
     })
 }
+
+
+
